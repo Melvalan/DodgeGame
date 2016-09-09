66400fcc265586fe13b39f8c6f6d188e6a7c3447
Christian Enlund <christian.enlund@hotmail.com>
2016-09-10 01:42:57 +0300

    Begiinsings

---
+++ DodgeGame/DodgeGameMain.cs
@@ -0,0 +1,23 @@
+﻿using System; // We need the standard .NET library for many things
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodgeGameMain
+{
+    class DodgeGameMain
+    {
+        static void Main()
+        {
+
+            Unit playerUnit = new Unit();
+
+            Console.SetCursorPosition(playerUnit.x, playerUnit.y);
+            Console.Write(playerUnit.unitGraphic);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DodgeGame/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodgeGame
+{
+    public class Unit
+    {
+        int x;
+        int y;
+        string unitGraphic;
+    }
+}
