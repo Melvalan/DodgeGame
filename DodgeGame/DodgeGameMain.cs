20c9a44971621e215530bdd1bac94d5d660dff11
Christian Enlund <christian.enlund@hotmail.com>
2016-09-10 01:56:10 +0300

    Starting to make progress

--- DodgeGame/DodgeGameMain.cs
+++ DodgeGame/DodgeGameMain.cs
@@ -4,18 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DodgeGameMain
+namespace DodgeGame
 {
     class DodgeGameMain
     {
         static void Main()
         {
 
+            //Instantiate a Unit that will represent the player.
             Unit playerUnit = new Unit();
 
-            Console.SetCursorPosition(playerUnit.x, playerUnit.y);
-            Console.Write(playerUnit.unitGraphic);
+            // Set data for this player's Unit
+            playerUnit.unitGraphic = "@";
+            playerUnit.x = 10;
+            playerUnit.y = 5;
 
+            Unit enemyUnit = new Unit();
+
+            // Instanciate the enemy
+            enemyUnit.unitGraphic = "X";
+            enemyUnit.x = 20;
+            enemyUnit.y = 17;
+
+            enemyUnit.Draw();
+            playerUnit.Draw();
+
+            // Program END 
+            // Do some clean-up
+
+            //Set cursor to bottom-most row
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
             Console.ReadLine();
 
         }
--- DodgeGame/Unit.cs
+++ DodgeGame/Unit.cs
@@ -8,8 +8,18 @@ namespace DodgeGame
 {
     public class Unit
     {
-        int x;
-        int y;
-        string unitGraphic;
+        public int x;
+        public int y;
+        public string unitGraphic;
+
+        // This draws the unit on the screen.
+        public void Draw()
+        {
+            // This is an instance method, so if we refer to fields like x and y
+            // we will be using the values that belong to this instance and this 
+            // instance only!
+            Console.SetCursorPosition(x, y);
+            Console.Write(unitGraphic);
+        }
     }
 }
